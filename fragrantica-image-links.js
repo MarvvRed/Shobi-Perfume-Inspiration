ba2b7534106cdf33c5776bacb24baa4c612a728c
MarvvRed <eronius85@gmail.com>
2026-08-19 14:28:13 +0200

    Extend Fragrantica image links through bestseller 20

--- fragrantica-image-links.js
+++ fragrantica-image-links.js
@@ -1,4 +1,4 @@
-// Best sellers #1-#6: make the bottle image an explicit link to the matching Fragrantica page.
+// Best sellers #1-#20: make the bottle image an explicit link to the matching Fragrantica page.
 (function(){
   if(typeof renderVanillaPrototype!=='function') return;
 
@@ -8,7 +8,21 @@
     'https://www.fragrantica.com/perfume/Byredo/Blanche-6686.html',
     'https://www.fragrantica.com/perfume/Tom-Ford/Tobacco-Vanille-1825.html',
     'https://www.fragrantica.com/perfume/ZARKOPERFUME/The-Muse-60665.html',
-    'https://www.fragrantica.com/perfume/Maison-Francis-Kurkdjian/Baccarat-Rouge-540-33519.html'
+    'https://www.fragrantica.com/perfume/Maison-Francis-Kurkdjian/Baccarat-Rouge-540-33519.html',
+    'https://www.fragrantica.com/perfume/Creed/Virgin-Island-Water-899.html',
+    'https://www.fragrantica.com/perfume/Tom-Ford/Lost-Cherry-51411.html',
+    'https://www.fragrantica.com/perfume/Dolce-Gabbana/Devotion-84951.html',
+    'https://www.fragrantica.com/perfume/Sol-de-Janeiro/Brazilian-Crush-Cheirosa-62-56062.html',
+    'https://www.fragrantica.com/perfume/Tom-Ford/Soleil-Blanc-34893.html',
+    'https://www.fragrantica.com/perfume/Byredo/Bal-d-Afrique-6458.html',
+    'https://www.fragrantica.com/perfume/Creed/Aventus-9828.html',
+    'https://www.fragrantica.com/perfume/By-Kilian/Love-Don-t-Be-Shy-4322.html',
+    'https://www.fragrantica.com/perfume/Zadig-Voltaire/This-is-Her-39358.html',
+    'https://www.fragrantica.com/perfume/Matiere-Premiere/Vanilla-Powder-84933.html',
+    'https://www.fragrantica.com/perfume/Giardini-Di-Toscana/Bianco-Latte-64757.html',
+    'https://www.fragrantica.com/perfume/Xerjoff/Naxos-30529.html',
+    'https://www.fragrantica.com/perfume/Diptyque/Philosykos-Eau-de-Parfum-3865.html',
+    'https://www.fragrantica.com/perfume/Burberry/Goddess-83483.html'
   ];
 
   const baseRender=renderVanillaPrototype;
