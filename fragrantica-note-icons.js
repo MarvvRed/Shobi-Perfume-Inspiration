233a552f0ae8adc3c0e5d6ad23ecd75921f7995a
MarvvRed <eronius85@gmail.com>
2026-08-22 02:18:44 +0200

    Add Fragrantica note icon registry runtime

---
+++ fragrantica-note-icons.js
@@ -0,0 +1,3 @@
+// Generated registry placeholder. The sync workflow replaces this from https://www.fragrantica.com/notes/.
+window.FRAGRANTICA_NOTE_ICON_IDS=window.FRAGRANTICA_NOTE_ICON_IDS||{};
+window.FRAGRANTICA_NOTE_ICON_REGISTRY_META=window.FRAGRANTICA_NOTE_ICON_REGISTRY_META||{source:'https://www.fragrantica.com/notes/',count:0};
